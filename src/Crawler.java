9d1c9931d6e2b892f84a00fb0ceab43912d0a8e4
onncho <onncho@Onns-MacBook-Pro.local>
2016-02-11 15:06:01 +0200

    port scan

--- src/Crawler.java
+++ src/Crawler.java
@@ -9,12 +9,15 @@ public class Crawler {
 	int m_NumOfAnalyzers;
 	String m_DomainToCrawl;
 	Downloader m_Downloader;
+	PortScanner m_PortScanner;
 	
 	LinkedList<LinkReport> m_Reports;
 	CrawlerReport m_FinalReport;
+	boolean m_RobotsChecked;
+	boolean m_PortScannerChecked;
 
 	// TODO: Need to receive parameters 
-	public Crawler(String i_DomainToCrawl) {
+	public Crawler(String i_DomainToCrawl, boolean i_RobotsChecked, boolean i_PortScanChecked) {
 		// TODO Auto-generated constructor stub
 		m_Reports = new LinkedList<>();
 		m_NumOfDownloaders = Integer.parseInt(ConfigurationObject.getMaxDownloaders());
@@ -23,17 +26,26 @@ public class Crawler {
 		m_HtmlsQueueToAnalyze = new SynchronizedQueueLL();
 		m_threadPool = new ThreadPoolV1(m_UrlsQueueToDownload, m_NumOfDownloaders, 
 				m_HtmlsQueueToAnalyze, m_NumOfAnalyzers);
+		m_RobotsChecked = i_RobotsChecked;
+		m_PortScannerChecked = i_PortScanChecked;
 		
+		// start
 		startCrawling();
-		
 	}
 
 	private void startCrawling() {
 		// TODO Auto-generated method stub
 		
 		// if port scan V -> DO First
+		if (m_PortScannerChecked) {
+			//TODO: change ports
+			m_PortScanner = new PortScanner(m_DomainToCrawl, 80, 200);
+		}
 		
-		// if robots -> do something 
+		// if robots -> do something
+		if (m_RobotsChecked) {
+			//TODO: add robots
+		}
 		
 		m_Downloader = new Downloader(m_threadPool, m_DomainToCrawl);
 		m_threadPool.putTaskInDownloaderQueue((Runnable) m_Downloader);
--- src/PortScanner.java
+++ src/PortScanner.java
@@ -5,54 +5,68 @@ import java.net.UnknownHostException;
 import java.util.ArrayList;
 
 public class PortScanner {
+
+	//TODO: convert to multi-threaded
+
+	String m_target;
+	int m_StartPort;
+	int m_EndPort;
+	ArrayList<Integer> m_OpenPorts;
+	private final String m_RegexDefinition; 
+	private final String m_RegexHttp;
+	private String m_TargetParsed;
 	
-	
-	public static ArrayList<Integer> runScan(String i_Target, int i_StartPort, int i_EndPort) {
-		
-		ArrayList<Integer> openPorts = new ArrayList<>();
-		String regexDefinition = "^.[0-9]{1,3}/..[0-9]{1,3}/..[0-9]{1,3}/..[0-9]{1,3}";
-		String regexHttp = "http://";
-		String targetParsed = null;
-		
+	public PortScanner(String i_Target, int i_StartPort, int i_EndPort) {
+		m_target = i_Target;
+		m_StartPort = i_StartPort;
+		m_EndPort = i_EndPort;
+		m_RegexDefinition = "^.[0-9]{1,3}/..[0-9]{1,3}/..[0-9]{1,3}/..[0-9]{1,3}";
+		m_RegexHttp = "http://";
+		m_TargetParsed = null;
+		getIpFromHost();
+	}
+
+	private void getIpFromHost() {
 		// if target is ip version 4
-		if (i_Target.matches(regexDefinition) == false) {			
+		if (m_target.matches(m_RegexDefinition) == false) {		
 			
-			i_Target = i_Target.replaceFirst(regexHttp, "");
-			
+			m_target = m_target.replaceFirst(m_RegexHttp, "");
 			InetAddress address;
-			
+
 			try {
-				address = InetAddress.getByName(i_Target);
-				targetParsed = address.getHostAddress();
+				address = InetAddress.getByName(m_target);
+				m_TargetParsed = address.getHostAddress();
 			} catch (UnknownHostException e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
 		}
-		
-	
-		for (int port = i_StartPort; port < i_EndPort; port++) {
-			
+	}
+
+	public ArrayList<Integer> runScan() {
+		 
+		for (int port = m_StartPort ; port < m_EndPort; port++) {
+
 			try 
 			{	
 				Socket socket = new Socket();
-				
-				if (targetParsed != null) {					
-					socket.connect(new InetSocketAddress(targetParsed, port), 1000);
+
+				if (m_TargetParsed != null) {					
+					socket.connect(new InetSocketAddress(m_TargetParsed, port), 1000);
 				}
-				
-				openPorts.add(port);
+
+				m_OpenPorts.add(port);
 				socket.close();
 			} 
 			catch (Exception e) 
 			{
 				System.out.println("Port : " + port + " is Closed");
 			}
-			
+
 		}
-		
-		return openPorts;
-		
+
+		return m_OpenPorts;
+
 	}
-	
+
 }
