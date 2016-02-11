3eab930c5cde47a43ec548b9d95c389d877b2d4f
galrettig@gmail.com <galrettig@gmail.com>
2016-02-11 02:16:24 +0200

    removed old version of finding tags

--- src/AnalyzerTask.java
+++ src/AnalyzerTask.java
@@ -117,40 +117,6 @@ public class AnalyzerTask implements Runnable {
 	}
 	
 
-	private void getAllAnchorsFromSource(){
-		String subjectTag = "<a";
-		String propertyToSearchFor = "href=";
-
-		int currentIndex = m_htmlSourceCode.indexOf(subjectTag);
-
-		// while there are still anchors from currentIndex to end of the string..
-		while(currentIndex > -1){
-			String link = null;
-			char kindOfQuoteCharUsed;
-
-			//indexes of the link itself aka -> <a href='www.someLink.com'
-			int linkStartIndex, linkEndIndex;
-
-			//inside an "<a" tag there is the "href=" property that holds the link address
-			int hrefIndexInAnchor = m_htmlSourceCode.indexOf(propertyToSearchFor, currentIndex);
-
-			linkStartIndex = (hrefIndexInAnchor + propertyToSearchFor.length());
-
-			//can identifiy links with ' or " char, inorder to fecth it correctly 
-			kindOfQuoteCharUsed = m_htmlSourceCode.charAt(linkStartIndex);
-
-			//pointing to the closing quote char
-			linkEndIndex = m_htmlSourceCode.indexOf(kindOfQuoteCharUsed, linkStartIndex + 1);
-
-			if(linkStartIndex > -1 && linkEndIndex > -1){
-				link = m_htmlSourceCode.substring(linkStartIndex, linkEndIndex);
-				m_anchors.push(this.removeQuoteCharFromString(link));
-			}
-
-			currentIndex = m_htmlSourceCode.indexOf(subjectTag, currentIndex + subjectTag.length());
-		}
-	}
-	
 	
 	private int populateCorrectList(String linkToMap){
 		String ext = getExtensionFromString(linkToMap);
